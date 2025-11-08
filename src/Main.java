a50b60b35a3c8b70ded36b38dad65319e0a6bddc
Vladislav Vaskin <vvaskin@inno.tech>
2025-11-08 15:35:44 +0300

    New feature added:int Numbers calculation

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        System.out.println("Введите первое целое число:");
+        int number1 = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе целое число:");
+        int number2 = new Scanner(System.in).nextInt();
+        int firstResult = number1 + number2;
+        int secondResult = number1 - number2;
+        int thirdResult = number1 * number2;
+        double fourthResult = (double) number1 / number2;
+        System.out.println("Сумма:" + firstResult);
+        System.out.println("Разность:" + secondResult);
+        System.out.println("Произведение:" + thirdResult);
+        System.out.println("Частное:" + fourthResult);
     }
 }
